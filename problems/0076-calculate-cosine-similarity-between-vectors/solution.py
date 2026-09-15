dfba09342d33a05bf4b820ca565c61e03429908b
ebsc0 <37756409+ebsc0@users.noreply.github.com>
2026-09-15 21:17:05 +0000

    Solve: Calculate Cosine Similarity Between Vectors

---
+++ problems/0076-calculate-cosine-similarity-between-vectors/solution.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def cosine_similarity(v1, v2):
+	"""
+	Calculate the cosine_similarity of two vectors.
+	Args:
+		vec1 (numpy.ndarray): 1D array representing the first vector.
+		vec2 (numpy.ndarray): 1D array representing the second vector.
+	Returns:
+		The cosine_similarity of the two vectors.
+	"""
+
+	return float(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
